ddb0359209898aab345b2e4fdfe9c2e4987e25bf
Luís Piteira <lcpiteira@gmail.com>
2022-09-13 15:46:27 +0100

    Spring initializr Project in Java 8

---
+++ src/main/java/com/springlearning/guru/springmasterj8/Springmasterj8Application.java
@@ -0,0 +1,13 @@
+package com.springlearning.guru.springmasterj8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springmasterj8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springmasterj8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springlearning/guru/springmasterj8/Springmasterj8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springlearning.guru.springmasterj8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springmasterj8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
